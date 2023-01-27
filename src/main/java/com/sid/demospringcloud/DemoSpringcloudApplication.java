5aa421ab73810ce98573a7df4f7a39e15c4e2409
AbouzyaKarim <kabouzya@gmail.com>
2023-01-27 22:45:17 +0100

    First Commit

---
+++ src/main/java/com/sid/demospringcloud/DemoSpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.sid.demospringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringcloudApplication.class, args);
+    }
+
+}
